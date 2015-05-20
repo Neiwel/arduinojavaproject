0849e757c7fcdde8efaad7933570838571f648dc
gaston <gaston.estevez7@gmail.com>
2015-05-20 16:08:02 -0300

    agrego tests y enum con tipo de medicion

---
+++ src/tests/DataBase.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.Test;
+
+public class DataBase {
+
+	
+	@Test
+	public void insertarUnDato(){
+		
+	}
+	
+	@Test
+	public void obtenerUnDato(){
+		
+	}
+	
+	@Test
+	public void obtenerTodosLosDatos(){
+		
+	}
+}
