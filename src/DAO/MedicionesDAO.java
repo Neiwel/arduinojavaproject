ae9d9dfcd5f7afc1800d9c713e0658281eb52e4f
gaston <gaston.estevez7@gmail.com>
2015-05-20 15:55:54 -0300

    agregue punto y coma

--- src/DAO/MedicionesDAO.java
+++ src/DAO/MedicionesDAO.java
@@ -7,6 +7,6 @@ public interface MedicionesDAO {
 	
 	public void insertarDatos(Mediciones mediciones) throws MedicionesException;
 	public void borrarDatos(Mediciones mediciones);
-	public void actualizarDatos(Mediciones mediciones);
+	public void actualizarDatos(Mediciones mediciones);;;;
 	
 }
