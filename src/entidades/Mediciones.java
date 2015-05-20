9158d7c2b7ad0375241dd0b7d8b75439caebc8de
Neiwel <nahuellmg@gmail.com>
2015-05-20 14:46:54 -0300

    agergue clase

--- src/entidades/Mediciones.java
+++ src/entidades/Mediciones.java
@@ -3,5 +3,42 @@ package entidades;
 public class Mediciones {
 	private String nombre;
 	private String tipo;
-	private int ente
+	private double valor;
+	
+	
+	
+	
+	
+	// Metodos
+	
+	public String getNombre(){
+		return this.nombre;
+	}
+	
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	
+	public String getTipo(String tipo){
+		return this.tipo;
+	}
+	
+	public void setTipo(String tipo){
+		this.tipo = tipo;
+	}
+	
+	public Double getValor(double valor){
+		return this.valor;
+	}
+	
+	public void setValor(double valor){
+		this.valor = valor;
+	}
+	
+	@Override
+	public String toString() {
+		return "Medicion [nombre=" + nombre + ", tipo=" + tipo
+				+ ", valor" + valor + "]";
+	}
+	
 }
